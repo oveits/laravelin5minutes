8937d457e052296ce77302fa1b4900849f88b70b
Oliver Veits <oliver.veits@web.de>
2015-02-09 09:11:51 +0000

    second commit, after 'git add .'

---
+++ bootstrap/autoload.php
@@ -0,0 +1,85 @@
+<?php
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Register The Composer Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader
+| for our application. We just need to utilize it! We'll require it
+| into the script here so that we do not have to worry about the
+| loading of any our classes "manually". Feels great to relax.
+|
+*/
+
+// OV next require statement commented out because of following error:
+/* 
+Fatal error: require(): Failed opening required /'/home/action/workspace/laravelin5minutes/bootstrap/../vendor/autoload.php' (include_path='.:/home/action/.parts/packages/php5/5.5.11/lib/php') in /home/action/workspace/laravelin5minutes/bootstrap/autoload.php on line 17
+*/
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Include The Compiled Class File
+|--------------------------------------------------------------------------
+|
+| To dramatically increase your application's performance, you may use a
+| compiled class file which contains all of the classes commonly used
+| by a request. The Artisan "optimize" is used to create this file.
+|
+*/
+
+if (file_exists($compiled = __DIR__.'/compiled.php'))
+{
+	require $compiled;
+}
+
+/*
+|--------------------------------------------------------------------------
+| Setup Patchwork UTF-8 Handling
+|--------------------------------------------------------------------------
+|
+| The Patchwork library provides solid handling of UTF-8 strings as well
+| as provides replacements for all mb_* and iconv type functions that
+| are not available by default in PHP. We'll setup this stuff here.
+|
+*/
+
+/* OV Patchwork commented out because of following error:
+Fatal error: Class 'Patchwork\Utf8\Bootup' not found in /home/action/workspace/laravelin5minutes/bootstrap/autoload.php on line 50
+*/
+Patchwork\Utf8\Bootup::initMbstring();
+
+/*
+|--------------------------------------------------------------------------
+| Register The Laravel Auto Loader
+|--------------------------------------------------------------------------
+|
+| We register an auto-loader "behind" the Composer loader that can load
+| model classes on the fly, even if the autoload files have not been
+| regenerated for the application. We'll add it to the stack here.
+|
+*/
+
+/* OV Illuminate commented out because of error
+Fatal error: Class 'Illuminate\Support\ClassLoader' not found in /home/action/workspace/laravelin5minutes/bootstrap/autoload.php on line 66
+*/
+Illuminate\Support\ClassLoader::register();
+
+/*
+|--------------------------------------------------------------------------
+| Register The Workbench Loaders
+|--------------------------------------------------------------------------
+|
+| The Laravel workbench provides a convenient place to develop packages
+| when working locally. However we will need to load in the Composer
+| auto-load files for the packages so that these can be used here.
+|
+*/
+
+if (is_dir($workbench = __DIR__.'/../workbench'))
+{
+	Illuminate\Workbench\Starter::start($workbench);
+}
